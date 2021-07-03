789176fe8c3bcc3e1c6173b5e76ed06c864230d1
Erisoka <leonidas.asprilla@outlook.com>
2021-07-02 20:59:34 -0500

    Publicado Jetstream, UsersTableSeeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'id'             => 1,
+                'name'           => 'Leonidas',
+                'email'          => 'leonidas@laravel.com',
+                'password'       => Hash::make('Omicron151181*'),
+                'remember_token' => null,
+            ],
+            [
+                'id'             => 2,
+                'name'           => 'User',
+                'email'          => 'user@user.com',
+                'password'       => bcrypt('password'),
+                'remember_token' => null,
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
